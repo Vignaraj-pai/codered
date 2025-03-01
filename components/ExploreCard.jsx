6b27f1b9101466783935c38e57001afbd2a6c862
Vignaraj <vignarajpai@outlook.com>
2025-03-01 14:47:34 +0530

    fix lint issues

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -4,7 +4,6 @@
 
 import { useState } from 'react';
 import { motion } from 'framer-motion';
-import styles from '../styles';
 import { fadeIn } from '../utils/motion';
 
 const ExploreCard = ({ id, imgUrl, title, shortDescription, description, objective, link, index, active, handleClick }) => {
